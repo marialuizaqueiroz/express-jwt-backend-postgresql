8cf760bd63615ff22deafb83154f7621043da951
marialuizaqueiroz <marialuqrls2005@gmail.com>
2025-10-22 11:26:05 -0300

    ctrl z no database/index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,20 +4,10 @@ import logger from '../utils/logger';
 
 export const connectDB = async () => {
   try {
-    logger.info('Attempting MongoDB connection...');
-    
-    const options = {
-      bufferCommands: false,
-    };
-
-    await mongoose.connect(config.mongoUri, options);
-
+    await mongoose.connect(config.mongoUri);
     logger.info('MongoDB connected');
-
-  } catch (error) {
-    logger.error('!!!!!!!!!! FATAL MONGODB CONNECTION ERROR !!!!!!!!!!!');
-    logger.error(error); 
-    
-    process.exit(1); 
+  } catch (error: any) {
+    logger.error('MongoDB connection error', error.message || error);
+    process.exit(1);
   }
 };
