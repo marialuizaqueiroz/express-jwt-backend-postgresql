da81c3da5e7c440b3b4fcd247471b7916a0b5555
marialuizaqueiroz <marialuqrls2005@gmail.com>
2025-11-07 14:42:10 -0300

    tentando corrigir documentação swagger

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use("/api", authRouter);
 app.use("/api/tasks", taskRoutes); 
-
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(errorMiddleware);
 try {
